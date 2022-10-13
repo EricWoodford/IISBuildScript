4ce10361196fc3652ad351ed8ae2a9fd75bced49
Eric Woodford <39199956+EricWoodford@users.noreply.github.com>
2022-10-13 16:47:41 -0700

    added HELP pages, and provided examples on use.

--- InstallIIS-2019.ps1
+++ InstallIIS-2019.ps1
@@ -1,21 +1,74 @@
+<# 
+.SYNOPSIS 
+    enable IIS and FTP on local machine
+
+.DESCRIPTION 
+    Script will enable the windows features, then if optional parameters are specified, it will configure IIS to meet those needs.
+
+.NOTES 
+    
+ 
+.COMPONENT 
+    Requires Module ActiveDirectory
+ 
+.LINK 
+    https://github.com/scriptrunner/ActionPacks/tree/master/ActiveDirectory/Users 
+
+.Parameter ConfigureWebsite
+    Include this switch to create the web folder on the drive and folder specified.
+
+.Parameter Web_drive_letter
+    drive letter for the web site's default install folder.  (D, E..)
+
+.Parameter web_path
+    folder where IIS will be configured to look for new websites. Default value is 'CA.Web'
+ 
+.Parameter AdminGroup
+    The security group that will be granted permissions to the website folder. Default value is 'TDC\WebServerAdmin Group'
+
+.Parameter ConfigureFTP
+    Include this switch to configure default FTP server.
+
+.Parameter RemoveWeb
+    Include this switch to remove the web feature from the server.
+
+.Parameter RemoteFTP
+    Include this switch to remove the FTP feature from the server. 
+    
+.EXAMPLE
+    .\installIIS-2019.ps1 
+    Installs IIS feature 
+
+.EXAMPLE
+    .\InstallIIS-2019.ps1 -ConfigureWebSite -web_drive_letter D
+    Install IIS feature, creates folder on drive D called CA.Web and configures permissions for the TDC\WebServerAdmin Group to the folder. 
+
+.EXAMPLE
+    .\InstallIIS-2019.ps1 -ConfigureWebSite -web_drive_letter D -web_path 'EDDWebServer' -adminGroup 'edd\webAdmins' -configureFTP
+    Install IIS feature, creates folder on drive D called EDDWebServer and configures permissions for the EDD\WebAdmins Group to the folder. In addition, enables the FTP service and grants the EDD\WebAdmins group permissions.     
+#>
+
+
 [CmdLetBinding()]
 param( 
+    [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
+    [switch]$configureWebsite,
+    [parameter(Mandatory = $false)]    
+    [switch]$removeWeb,    
+    [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
+    [string]$Web_Drive_letter,
+    [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
+    [string]$Web_Path = "CA.Web",
+    [parameter(Mandatory = $false)]    
+    [string]$AdminGroup = "TDC\WebServerAdmin Global",
     [parameter(Mandatory = $false)]
     [switch]$ConfigureFTP,
-    [parameter(Mandatory = $false,ParameterSetName="configWeb")]
-    [switch]$configureWebsite,
-    [parameter(Mandatory = $false,ParameterSetName="configWeb")]
-    [string]$Web_Drive_letter,
-    [parameter(Mandatory = $false,ParameterSetName="configWeb")]
-    [string]$Web_Path = "CA.Web",
-    [parameter(Mandatory = $false)]
-    [string]$AdminGroup = "TDC\WebServerAdmin Global",
-    [parameter(Mandatory = $false)]    
-    [switch]$removeWeb,
     [parameter(Mandatory = $false)]    
     [switch]$removeFTP
-
 )
+# with no switches, the script will install the windows features for IIS
+# when variables are specified it will configure different items on the server. 
+
 
 # variable used to track if a reboot is required after any portion of script. 
 $restartNeeded = $false
@@ -29,16 +82,16 @@ $FTPService = Get-Service -name FTPSV -ErrorAction SilentlyContinue
 #get-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" 
 
 ### Install IIS and features ###
-import-module servermanager,WebAdministration
+import-module servermanager, WebAdministration
 if ($removeWeb) {
     $CaptureInstall = remove-WindowsFeature Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console 
     $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
 } 
 
-if ($null -eq $webService)  {
-    $CaptureInstall= Add-WindowsFeature Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console 
+if ($null -eq $webService) {
+    $CaptureInstall = Add-WindowsFeature Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console 
     $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
-    if ($catpureInstall.exitCode -ne "Success") { return "failed to install Windows Features"}
+    if ($catpureInstall.exitCode -ne "Success") { return "failed to install Windows Features" }
 }
 
 
@@ -82,38 +135,38 @@ if ($ConfigureFTP) {
     set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/ssl -name ssl128 -value True
     ###$hostname = hostname
     ###$cert = get-childitem cert:/localmachine/my | where-object {$_.subject -eq "CN=WMSvc-$hostname"}
-    $cert = get-childitem cert:/localmachine/my | where-object {$_.subject -eq "CN=WMSVC-SHA2"}
+    $cert = get-childitem cert:/localmachine/my | where-object { $_.subject -eq "CN=WMSVC-SHA2" }
     set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/ssl -name serverCertHash -value $cert.thumbprint
-    Add-WebConfiguration -Filter /System.FTPServer/Security/Authorization -PSPath 'IIS:\' -Value (@{AccessType="Allow"; Users=$AdminGroup; Roles=$AdminGroup; Permissions="Read, Write"})
+    Add-WebConfiguration -Filter /System.FTPServer/Security/Authorization -PSPath 'IIS:\' -Value (@{AccessType = "Allow"; Users = $AdminGroup; Roles = $AdminGroup; Permissions = "Read, Write" })
 }
 
 if ($configureWebsite) {
-    if ($Web_Drive_letter -like "*:") { $Web_Drive = $Web_Drive_letter +"\"} 
+    if ($Web_Drive_letter -like "*:") { $Web_Drive = $Web_Drive_letter + "\" } 
     else { $web_drive = $(get-psDrive -name $web_drive_letter).root }
-    if ($null -eq $web_drive -or $Web_Drive -eq "") { return "invalid drive requested"}
+    if ($null -eq $web_drive -or $Web_Drive -eq "") { return "invalid drive requested" }
 
-    $Web_folder = $web_drive+$Web_Path
+    $Web_folder = $web_drive + $Web_Path
     ### Create WAU folder structure ###
-    if (!(test-path -path $web_folder)) {New-Item -Type Directory -Path $web_drive -Name $Web_Path}
-    if (!(test-path -path $($web_folder+"/certs"))) {New-Item -Type Directory -Path $Web_folder -Name Certs}
-    if (!(test-path -path $($web_folder+"/LogFiles"))) {New-Item -Type Directory -Path $Web_folder -Name LogFiles}
-    if (!(test-path -path $($web_folder+"/Sites"))) {New-Item -Type Directory -Path $Web_folder -Name Sites}
-    if (!(test-path -path $($web_folder+"/Utils"))) {New-Item -Type Directory -Path $Web_folder -Name Utils}
-    compact.exe /c /s $($Web_folder+"\LogFiles")
-    Attrib.exe +i $($Web_folder+"\LogFiles")
+    if (!(test-path -path $web_folder)) { New-Item -Type Directory -Path $web_drive -Name $Web_Path }
+    if (!(test-path -path $($web_folder + "/certs"))) { New-Item -Type Directory -Path $Web_folder -Name Certs }
+    if (!(test-path -path $($web_folder + "/LogFiles"))) { New-Item -Type Directory -Path $Web_folder -Name LogFiles }
+    if (!(test-path -path $($web_folder + "/Sites"))) { New-Item -Type Directory -Path $Web_folder -Name Sites }
+    if (!(test-path -path $($web_folder + "/Utils"))) { New-Item -Type Directory -Path $Web_folder -Name Utils }
+    compact.exe /c /s $($Web_folder + "\LogFiles")
+    Attrib.exe +i $($Web_folder + "\LogFiles")
 
-    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name directory -value $($Web_folder+"\LogFiles")
-    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name directory -value $($Web_folder+"\LogFiles")
+    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name directory -value $($Web_folder + "\LogFiles")
+    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name directory -value $($Web_folder + "\LogFiles")
 
     ### Set permissions oN WAU folder Structure ###
 
     $acl = Get-Acl $Web_folder
     $acl.SetAccessRuleProtection($True, $False)
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators","FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
     $acl.AddAccessRule($rule)
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("System","FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("System", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
     $acl.AddAccessRule($rule)
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($AdminGroup,"FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($AdminGroup, "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
     $acl.AddAccessRule($rule)
     Set-Acl $Web_folder $acl
 }

e3abefefdb0d48f285c700de2584242e0f70bba5
Eric Woodford <39199956+EricWoodford@users.noreply.github.com>
2023-08-02 14:29:45 -0700

    fixed missing variable

--- InstallIIS-2019.ps1
+++ InstallIIS-2019.ps1
@@ -51,33 +51,27 @@
 
 [CmdLetBinding()]
 param( 
-   # [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
-   # [switch]$configureWebsite,
+    # [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
+    # [switch]$configureWebsite,
     [parameter(Mandatory = $false)]    
     [switch]$removeWeb,    
-   # [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
-   # [string]$Web_Drive_letter,
+    # [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
+    # [string]$Web_Drive_letter,
     [parameter(Mandatory = $false, ParameterSetName = "configWeb")]
     [string]$Web_Path = "CA.Web",
     [parameter(Mandatory = $false)]    
     [string]$AdminGroup = "TDC\WebServerAdmin Global",
-   # [parameter(Mandatory = $false)]
-   # [switch]$ConfigureFTP,
+    # [parameter(Mandatory = $false)]
+    # [switch]$ConfigureFTP,
     [parameter(Mandatory = $false)]    
     [switch]$removeFTP
 )
 # with no switches, the script will install the windows features for IIS
 # when variables are specified it will configure different items on the server. 
 
-$configureWebsite = $configureFTP = !$removeWeb
-
-#if ($PSVersionTable.PSVersion.Major -lt 7) {
-#	write-verbose "installing posh 7"
-#	Invoke-Expression "& { $(Invoke-RestMethod -Uri https://aka.ms/install-powershell.ps1) } -UseMSI -Preview -Quiet"
-#}
 
 if ((get-packageProvider -name nuget).version.tostring() -lt "2.8.5") {
-	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208 -Force
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208 -Force
 }
 Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
@@ -86,151 +80,143 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 $restartNeeded = $false
 
 # creates log file based on current date-time.
+$LogRootFolder = "c:\bin"
 # c:\bin\iis_install-11-10-2022_104336_AM.log
-if (!(test-path -path "c:\bin\")) {
-	# bin folder doesn't exist, creating it for log drop.
-	new-item -path "c:\bin" -itemType Directory
+if (!(test-path -path $LogRootFolder)) {
+    # bin folder doesn't exist, creating it for log drop.
+    new-item -path $LogRootFolder -itemType Directory
 }
-$LogFile = "c:\bin\install-iis_"+$((get-date).ToString().replace("/","-").replace(" ","_").replace(":",""))+".log"
+$LogFile = $LogRootFolder + "\install-iis_" + $((get-date).ToString().replace("/", "-").replace(" ", "_").replace(":", "")) + ".log"
 Start-Transcript -Path $LogFile
 
 # check if web and FTP service already installed. 
 #$Name = "World Wide Web Publishing Service"
 $webService = Get-Service -name W3SVC -ErrorAction SilentlyContinue 
-$FTPService = Get-Service -name FTPSV -ErrorAction SilentlyContinue 
+# $FTPService = Get-Service -name FTPSV -ErrorAction SilentlyContinue 
 
 # might be useful to capture if reboot necessary. 
 # ref: https://devblogs.microsoft.com/scripting/use-powershell-to-find-servers-that-need-a-reboot/
 #get-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" 
 
-if ((get-command -Name "get-windowsFeature" -erroraction silentlycontinue) -eq $null) {
-	$Web_Path
-	### Install IIS and features ###
-	if ($null -eq (get-module serverManager)) {
-		install-module servermanager
-	}
-	if (get-module -name 'serverManager') {import-module servermanager}
+if ($null -eq (get-command -Name "get-windowsFeature" -erroraction silentlycontinue)) {
+    #	$Web_Path
+    ### Install IIS and features ###
+    if ($null -eq (get-module serverManager)) {
+        install-module servermanager
+    }
+    if (get-module -name 'serverManager') { import-module servermanager }
 }
 
 #list of features to install, will install individually. 
-$webFeatures = "Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console".split(",") | foreach{$_.trim()}
+$webFeatures = "Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console".split(",") | foreach { $_.trim() }
 
 if ($removeWeb) {
-    $CaptureInstall = $webFeatures | %{remove-WindowsFeature -name $_}  # remove-WindowsFeature Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console 
+    $CaptureInstall = $webFeatures | ForEach-Object { remove-WindowsFeature -name $_ }  # remove-WindowsFeature Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console 
     $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
 } 
 
 if ($null -eq $webService -and (-not $removeWeb)) {
     $InstallFeatures = $webFeatures
     do { 
-       # $CaptureInstall = $installFeatures| %{ Add-WindowsFeature -includeallSubFeature -name $_ }
-        $CaptureInstall = $installFeatures| %{ Add-WindowsFeature -name $_ }
-        $ConfirmInstall = $InstallFeatures | %{ get-WindowsFeature -name $_ }
-        $InstallFeatures = $ConfirmInstall.InstallState -eq "Available" | %{$_.name}
+        # $CaptureInstall = $installFeatures| %{ Add-WindowsFeature -includeallSubFeature -name $_ }
+        $CaptureInstall = $installFeatures | ForEach-Object { Add-WindowsFeature -name $_ }
+        $ConfirmInstall = $InstallFeatures | ForEach-Object { get-WindowsFeature -name $_ }
+        $InstallFeatures = $ConfirmInstall.InstallState -eq "Available" | ForEach-Object { $_.name }
     } while ($null -ne $installFeatures)
     # $CaptureInstall = $webFeatures | %{Add-WindowsFeature -includeallSubFeature -name $_}
-    $CaptureInstall = $webFeatures | %{Add-WindowsFeature -name $_}
+    $CaptureInstall = $webFeatures | ForEach-Object { Add-WindowsFeature -name $_ }
     $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
-    $confirmInstall = $webFeatures | %{get-WindowsFeature -name $_}
-    $ConfirmInstall | select name, installState | Write-Output
+    $confirmInstall = $webFeatures | ForEach-Object { get-WindowsFeature -name $_ }
+    $ConfirmInstall | Select-Object name, installState | Write-Output
     if ($null -ne ($ConfirmInstall.InstallState -ne "installed")) { return "failed to install Windows Features" }
-	# add .net components
-	write-output "install net-framework-core"
-	$captureInstall = Install-WindowsFeature Net-Framework-Core #-source \\network\share\sxs
-	$restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
+    # add .net components
+    write-output "install net-framework-core"
+    $captureInstall = Install-WindowsFeature Net-Framework-Core #-source \\network\share\sxs
+    $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
 
-	if ((get-WindowsFeature web-asp-net).installstate -ne "Installed") {
-	    $captureInstall = install-windowsfeature web-asp-net
-	    $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
-	}
-	if ((get-WindowsFeature web-asp-net45).installstate -ne "Installed") {
-	    #Install-WindowsFeature Web-Asp-Net45 -source c:\windows\winsxs
-	    $captureInstall = install-windowsfeature Net-Framework-45-Core
-	    $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
-	}
+    if ((get-WindowsFeature web-asp-net).installstate -ne "Installed") {
+        $captureInstall = install-windowsfeature web-asp-net
+        $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
+    }
+    if ((get-WindowsFeature web-asp-net45).installstate -ne "Installed") {
+        #Install-WindowsFeature Web-Asp-Net45 -source c:\windows\winsxs
+        $captureInstall = install-windowsfeature Net-Framework-45-Core
+        $restartNeeded = $restartNeeded -or ($CaptureInstall.restartNeeded -eq "yes")
+    }
 
-} else {
-    $webFeatures = "Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console".split(",") | foreach{$_.trim()}
-     $webfeatures | %{ get-WindowsFeature -name $_ } | write-output
+}
+else {
+    $webFeatures = "Web-Server, Web-Http-Redirect, Web-ASP, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Includes, Web-Log-Libraries, Web-Http-Tracing, Web-Basic-Auth, Web-Windows-Auth, Web-IP-Security, Web-Url-Auth, Web-Scripting-Tools, Web-Mgmt-Service, Web-FTP-Server, Web-Ftp-Service, Web-Dyn-Compression, Web-Mgmt-Console".split(",") | foreach { $_.trim() }
+    $webfeatures | ForEach-Object { get-WindowsFeature -name $_ } | write-output
     "web services already installed" | Write-Output
 }
 
-if ((get-command -name "set-webconfigurationproperty" -erroraction silentlycontinue) -eq $null) {
-	# webAdministration module is added as part of IIS features. Doesn't exist before then. 
-	if ($null -eq (get-module WebAdministration)) {
-		install-module WebAdministration
-	}
-	if (get-module -name WebAdministration ) {import-module WebAdministration}
+if ($null -eq (get-command -name "set-webconfigurationproperty" -erroraction silentlycontinue)) {
+    # webAdministration module is added as part of IIS features. Doesn't exist before then. 
+    if ($null -eq (get-module WebAdministration)) {
+        install-module WebAdministration
+    }
+    if (get-module -name WebAdministration ) { import-module WebAdministration }
 }
 
-if (-not $removeWeb) {
-	### Remove default site and app pools ###
-	Remove-WebSite -name *
-	Remove-WebAppPool -name *
-	### Configure server level HTTP settings ###
-	set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name logExtFileFlags -value "Date,Time,ClientIP,UserName,ServerIP,Method,UriStem,UriQuery,HttpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,Cookie,Referer,ProtocolVersion,Host,HttpSubStatus"
-	set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name localTimeRollover -value "True"
-	set-webconfigurationproperty /system.applicationHost/log -name logInUTF8 -value "False"
-	set-webconfigurationproperty /system.webServer/security/authentication/anonymousAuthentication -name userName -value ""
-	set-webconfigurationproperty /system.applicationHost/applicationPools/applicationPoolDefaults -name managedRuntimeVersion -value "v4.0"
+### Remove default site and app pools ###
+Remove-WebSite -name *
+Remove-WebAppPool -name *
+### Configure server level HTTP settings ###
+set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name logExtFileFlags -value "Date,Time,ClientIP,UserName,ServerIP,Method,UriStem,UriQuery,HttpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,Cookie,Referer,ProtocolVersion,Host,HttpSubStatus"
+set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name localTimeRollover -value "True"
+set-webconfigurationproperty /system.applicationHost/log -name logInUTF8 -value "False"
+set-webconfigurationproperty /system.webServer/security/authentication/anonymousAuthentication -name userName -value ""
+set-webconfigurationproperty /system.applicationHost/applicationPools/applicationPoolDefaults -name managedRuntimeVersion -value "v4.0"
+
+### Configure sever level FTP settings ###
+"configuring website per request:" | Write-Output
+set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name logExtFileFlags -value "Date,Time,ClientIP,UserName,ServerIP,Method,UriStem,FtpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,Host,FtpSubStatus,Session,FullPath,Info,ClientPort"
+set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name localTimeRollover -value "True"    
+set-webconfigurationproperty /system.ftpserver/log -name logInUTF8 -value "False"
+set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/authentication/basicauthentication -name Enabled -value True
+set-webconfigurationproperty /system.ftpserver/firewallsupport -name LowDataChannelPort -value 50000
+set-webconfigurationproperty /system.ftpserver/firewallsupport -name HighDataChannelPort -value 50050
+set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/ssl -name ssl128 -value True
+###$hostname = hostname
+###$cert = get-childitem cert:/localmachine/my | where-object {$_.subject -eq "CN=WMSvc-$hostname"}
+$cert = get-childitem cert:/localmachine/my | where-object { $_.subject -eq "CN=WMSVC-SHA2" }
+set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/ssl -name serverCertHash -value $cert.thumbprint
+Add-WebConfiguration -Filter /System.FTPServer/Security/Authorization -PSPath 'IIS:\' -Value (@{AccessType = "Allow"; Users = $AdminGroup; Roles = $AdminGroup; Permissions = "Read, Write" })
+
+
+"configuring website per request:" | Write-Output
+$web_drive = (get-psDrive | Where-Object { $_.description -eq "AppsData" }).root
+
+if ($null -eq $web_drive -or $Web_Drive -eq "") { 
+    "Web drive not found" | write-output 
+    return "invalid drive requested" 
 }
 
-if ($ConfigureFTP) {
-    ### Configure sever level FTP settings ###
-    "configuring website per request:" | Write-Output
-    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name logExtFileFlags -value "Date,Time,ClientIP,UserName,ServerIP,Method,UriStem,FtpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,Host,FtpSubStatus,Session,FullPath,Info,ClientPort"
-    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name localTimeRollover -value "True"    
-    set-webconfigurationproperty /system.ftpserver/log -name logInUTF8 -value "False"
-    set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/authentication/basicauthentication -name Enabled -value True
-    set-webconfigurationproperty /system.ftpserver/firewallsupport -name LowDataChannelPort -value 50000
-    set-webconfigurationproperty /system.ftpserver/firewallsupport -name HighDataChannelPort -value 50050
-    set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/ssl -name ssl128 -value True
-    ###$hostname = hostname
-    ###$cert = get-childitem cert:/localmachine/my | where-object {$_.subject -eq "CN=WMSvc-$hostname"}
-    $cert = get-childitem cert:/localmachine/my | where-object { $_.subject -eq "CN=WMSVC-SHA2" }
-    set-webconfigurationproperty /system.applicationhost/sites/siteDefaults/ftpserver/security/ssl -name serverCertHash -value $cert.thumbprint
-    Add-WebConfiguration -Filter /System.FTPServer/Security/Authorization -PSPath 'IIS:\' -Value (@{AccessType = "Allow"; Users = $AdminGroup; Roles = $AdminGroup; Permissions = "Read, Write" })
-}
+$Web_folder = $web_drive + $Web_Path
+### Create WAU folder structure ###
+if (!(test-path -path $web_folder)) { New-Item -Type Directory -Path $web_drive -Name $web_Path }
+if (!(test-path -path $($web_folder + "/certs"))) { New-Item -Type Directory -Path $Web_folder -Name Certs }
+if (!(test-path -path $($web_folder + "/LogFiles"))) { New-Item -Type Directory -Path $Web_folder -Name LogFiles }
+if (!(test-path -path $($web_folder + "/Sites"))) { New-Item -Type Directory -Path $Web_folder -Name Sites }
+if (!(test-path -path $($web_folder + "/Utils"))) { New-Item -Type Directory -Path $Web_folder -Name Utils }
+compact.exe /c /s $($Web_folder + "\LogFiles")
+Attrib.exe +i $($Web_folder + "\LogFiles")
 
-if ($configureWebsite) {
-    "configuring website per request:" | Write-Output
-    # determine drive letter for web install. See if drive letter if valid entry.
-    #if ($Web_Drive_letter -like "*:") { $Web_Drive = $Web_Drive_letter + "\" } 
-    # else { 
-    
-    $web_drive = (get-psDrive | ?{$_.description -eq "AppsData"}).root
+set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name directory -value $($Web_folder + "\LogFiles")
+set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name directory -value $($Web_folder + "\LogFiles")
 
-    #$web_drive = $(get-psDrive -name $web_drive_letter).root # }
-    
-    if ($null -eq $web_drive -or $Web_Drive -eq "") { 
-        "Web drive not found" | write-output 
-        return "invalid drive requested" }
+### Set permissions oN WAU folder Structure ###
 
-    $Web_folder = $web_drive + $Web_Path
-    ### Create WAU folder structure ###
-    if (!(test-path -path $web_folder)) { New-Item -Type Directory -Path $web_drive -Name }
-    if (!(test-path -path $($web_folder + "/certs"))) { New-Item -Type Directory -Path $Web_folder -Name Certs }
-    if (!(test-path -path $($web_folder + "/LogFiles"))) { New-Item -Type Directory -Path $Web_folder -Name LogFiles }
-    if (!(test-path -path $($web_folder + "/Sites"))) { New-Item -Type Directory -Path $Web_folder -Name Sites }
-    if (!(test-path -path $($web_folder + "/Utils"))) { New-Item -Type Directory -Path $Web_folder -Name Utils }
-    compact.exe /c /s $($Web_folder + "\LogFiles")
-    Attrib.exe +i $($Web_folder + "\LogFiles")
-
-    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/logFile -name directory -value $($Web_folder + "\LogFiles")
-    set-webconfigurationproperty /system.applicationHost/sites/siteDefaults/ftpserver/logFile -name directory -value $($Web_folder + "\LogFiles")
-
-    ### Set permissions oN WAU folder Structure ###
-
-    $acl = Get-Acl $Web_folder
-    $acl.SetAccessRuleProtection($True, $False)
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
-    $acl.AddAccessRule($rule)
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("System", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
-    $acl.AddAccessRule($rule)
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($AdminGroup, "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
-    $acl.AddAccessRule($rule)
-    Set-Acl $Web_folder $acl
-}
+$acl = Get-Acl $Web_folder
+$acl.SetAccessRuleProtection($True, $False)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("System", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule($AdminGroup, "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.AddAccessRule($rule)
+Set-Acl $Web_folder $acl
 
 
 
